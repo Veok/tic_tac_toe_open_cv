4203e56c8846b4d203feaba0d9fff52a9a36ec4c
Lelental <p.lelental@gmail.com>
2017-04-13 10:40:27 +0200

    houghcircles

--- src/TicTacToe/Controller.java
+++ src/TicTacToe/Controller.java
@@ -1,11 +1,13 @@
 package TicTacToe;
 
 
+import com.sun.javafx.geom.Vec3f;
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
 import org.opencv.calib3d.Calib3d;
 import org.opencv.core.*;
 
+import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.videoio.VideoCapture;
 
@@ -14,6 +16,10 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 import static TicTacToe.VideoHandler.toFxImage;
+import static java.lang.Math.*;
+import static org.opencv.imgproc.Imgproc.*;
+import static org.opencv.imgproc.Imgproc.HoughLines;
+import static org.opencv.imgproc.Imgproc.HoughLinesP;
 
 public class Controller {
 
@@ -23,7 +29,11 @@ public class Controller {
     private VideoCapture videoCapture = new VideoCapture();
     private ScheduledExecutorService timer;
     private Mat mat = new Mat();
+    private Mat gray = new Mat();
     private boolean isRunning;
+    private Size sizeOfBoard = new Size(4, 4);
+    private MatOfPoint2f points = new MatOfPoint2f();
+
 
     public Controller() {
         this.isRunning = true;
@@ -35,8 +45,9 @@ public class Controller {
         if (isRunning) {
             Runnable runnable = () -> {
                 Mat image = getMat();
-                findBoard();
                 toFxImage(image);
+                //findBoard();
+                detectCircle();
                 VideoHandler.onFXThread(controller.getFrame().imageProperty(), toFxImage(image));
 
             };
@@ -55,19 +66,30 @@ public class Controller {
     }
 
     private void findBoard() {
-        Size sizeOfBoard = new Size(4, 4);
-        MatOfPoint2f points = new MatOfPoint2f();
-
-        final boolean boardCorners = Calib3d.findChessboardCorners(getMat(), sizeOfBoard, points);
+        boolean boardCorners = Calib3d.findChessboardCorners(getMat(), sizeOfBoard, points);
 
         if (boardCorners) {
-            Mat gray = new Mat();
-            Imgproc.cvtColor(getMat(),gray, Imgproc.COLOR_BGR2GRAY);
-            Imgproc.cornerSubPix(gray, points, new Size(11, 11), new Size(-1, 1),
+            cvtColor(getMat(), gray, COLOR_RGBA2GRAY);
+            cornerSubPix(gray, points, new Size(11, 11), new Size(-1, 1),
                     new TermCriteria(TermCriteria.EPS + TermCriteria.MAX_ITER, 30, 0.1));
             Calib3d.drawChessboardCorners(getMat(), sizeOfBoard, points, true);
         }
 
+    }
+
+    private void detectCircle() {
+        cvtColor(getMat(), gray, COLOR_RGBA2GRAY);
+        GaussianBlur(gray, gray, new Size(9, 9), 2, 2);
+        Mat circles = new Mat();
+        HoughCircles(gray, circles, CV_HOUGH_GRADIENT, 2, gray.rows() / 8, 100, 100, 20, 400);
+        for (int i = 0; i < circles.cols(); i++) {
+            double[] circle = circles.get(0, i);
+            Point pt = new Point(Math.round(circle[0]), Math.round(circle[1]));
+            int radius = (int) Math.round(circle[2]);
+            Imgproc.circle(getMat(), pt, radius, new Scalar(0, 255, 0), 1);
+            Imgproc.circle(getMat(), pt, radius, new Scalar(0, 0, 255), 2);
+
+        }
     }
 
     private ImageView getFrame() {
